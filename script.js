237c78cb1ecd0a6aea63397dc8ca911342457b2e
MKevin2 <matheus.santos1616@etec.sp.gov.br>
2025-02-27 20:36:26 -0300

    Alteração de proporções para desktop

--- script.js
+++ script.js
@@ -5,37 +5,40 @@ function moverSlide(direcao) {
     const totalSlides = slides.length;
     indice += direcao;
     if (indice < 0) {
-        indice = totalSlides - 1; // Vai para o último slide
+        indice = totalSlides - 1; // Direciona para slide anterior
     } else if (indice >= totalSlides) {
-        indice = 0; // Vai para o primeiro slide
+        indice = 0; // Direciona para o próximo slide 
     }
     atualizarCarrossel();
 }
+
 function atualizarCarrossel() {
     const container = document.querySelector(".carrossel-container");
     const larguraSlide = document.querySelector(".carrossel-slide").clientWidth;
     container.style.transform = `translateX(-${indice * larguraSlide}px)`;
 }
+
 // Função para a transição automática dos slides
 function transicaoAutomatica() {
     moverSlide(1);
 }
-// Transição automática a cada 8 segundos
+
+// Transição automática a cada 5 segundos
 setInterval(transicaoAutomatica, 5000);
 
 function calcularTempo() {
     // Data inicial: 16 de Janeiro de 2023
     const dataInicial = new Date('2023-01-16T00:00:00');
     
-    // Data e hora atual
+    // Variável recupera a data atual
     const agora = new Date();
     
-    // Usar objeto Intl.DateTimeFormat para cálculo exato
+    // O uso do objeto Intl.DateTimeFormat é para fazer o cálculo preciso
     let anos = agora.getFullYear() - dataInicial.getFullYear();
     let meses = agora.getMonth() - dataInicial.getMonth();
     let dias = agora.getDate() - dataInicial.getDate();
 
-    // Ajustar valores caso o dia ou mês sejam negativos
+    // Ajuste de valores caso o dia ou mês sejam negativos
     if (dias < 0) {
         meses -= 1;
         const ultimoDiaMesAnterior = new Date(agora.getFullYear(), agora.getMonth(), 0).getDate();
@@ -47,12 +50,12 @@ function calcularTempo() {
         meses += 12;
     }
 
-    // Obter as horas, minutos e segundos atuais
+    // Obtenção das horas, minutos e segundos atuais
     const horas = agora.getHours();
     const minutos = agora.getMinutes();
     const segundos = agora.getSeconds();
 
-    // Atualiza os valores no HTML
+    // Passa os valores recebidos para a página
     document.getElementById('anos').innerHTML = anos + '<div class="titulo">Anos</div>';
     document.getElementById('meses').innerHTML = meses + '<div class="titulo">Meses</div>';
     document.getElementById('dias').innerHTML = dias + '<div class="titulo">Dias</div>';
@@ -61,5 +64,5 @@ function calcularTempo() {
     document.getElementById('segundos').innerHTML = segundos + '<div class="titulo">Segundos</div>';
 }
 
-// Chama a função a cada segundo
+// Atualização a cada 1 segundo
 setInterval(calcularTempo, 1000);
